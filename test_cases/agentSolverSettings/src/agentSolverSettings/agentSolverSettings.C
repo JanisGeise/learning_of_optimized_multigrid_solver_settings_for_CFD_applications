2ab98b1d36229ad008d532013f2cf1fba2f459b7
Janis Geise <j.geise@tu-braunschweig.de>
2023-08-02 21:57:33 +0200

    first idea of DRL for GAMG solver settings for 'interpolateCorrection'

--- test_cases/agentSolverSettings/src/agentSolverSettings/agentSolverSettings.C
+++ test_cases/agentSolverSettings/src/agentSolverSettings/agentSolverSettings.C
@@ -31,6 +31,8 @@ License
 #include "volFields.H"
 #include "ListOps.H"
 #include "zeroGradientFvPatchField.H"
+#include "dictionary.H"
+#include "IOdictionary.H"
 
 // * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
 
@@ -45,91 +47,6 @@ namespace functionObjects
 
 
 // * * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * //
-void Foam::functionObjects::agentSolverSettings::computeResidualProperties(const word& fieldName)
-{
-    const Foam::dictionary& solverDict = mesh_.solverPerformanceDict();
-
-    const List<SolverPerformance<scalar>> sp
-    (
-        solverDict.lookup(fieldName)
-    );
-
-    // same solver (GAMG) for all iterations within the time step, so just take the name of the first one
-    const word& solverName = sp[0].solverName();
-    const scalar& initialResidual = component(sp[0].initialResidual(), 0);
-
-    // we need this to access the components of the dict, however we only have one component since pressure is scalar
-    const label validComponents(mesh_.validComponents<scalar>());
-
-    // initialize the variables for storing the properties of the residuals
-    int sumSolverIter = 0;
-    int maxSolverIter = 0;
-    scalar maxAbsConRate = 0.0;
-    scalar minAbsConRate = 42.0;
-
-    // convergence rate has one element less since we are computing the difference between two subsequent iterations
-    torch::Tensor convergenceRate = torch::zeros(sp.size()-1, torch::TensorOptions().dtype(torch::kFloat64));
-
-    // loop over the residuals of each GAMG call and compute the quantities of interest
-    // TODO: check if we really only have one GAMG call per pimple iteration, otherwise we can't compute the convergence rate correctly
-    for (label i = 0; i < sp.size()-1; i++){
-        // since we are only using p, we only have one component, which is the first one
-        const scalar nIterations = component(sp[i].nIterations(), 0);
-
-        // update the max. amount of iterations of GAMG per PIMPLE call
-        if (nIterations > maxSolverIter){
-            maxSolverIter = nIterations;
-        }
-
-        // update sum of GAMG iterations
-        sumSolverIter += nIterations;
-
-        // compute the convergence rate
-        convergenceRate[i] = component(sp[i].initialResidual(), 0) - component(sp[i+1].initialResidual(), 0);
-
-        // save the max. convergence rate, magnitude because convergence rate is negative
-        if (mag(convergenceRate[i].item<double>()) > maxAbsConRate){
-            maxAbsConRate = mag(convergenceRate[i].item<double>());
-        }
-
-        // save the min. convergence rate, magnitude because convergence rate is negative
-        if (mag(convergenceRate[i].item<double>()) < minAbsConRate){
-            minAbsConRate = mag(convergenceRate[i].item<double>());
-        }
-    }
-
-    // for simplicity, for now just compute the avg. convergence rate instead of median
-    scalar avgAbsConRate = torch::mean(convergenceRate).abs().item<double>();
-
-    file() << token::TAB << solverName;
-
-    // for now only write these quantities to the file, later this will be additionally used as policy input
-    if (component(validComponents, 0) != -1)
-    {
-        // we only have one GAMG call per PIMPLE iteration, otherwise computing the convergence rate would not work
-        const int pimple_iter = sp.size();
-
-        file()
-            << token::TAB << initialResidual
-            << token::TAB << avgAbsConRate
-            << token::TAB << maxAbsConRate
-            << token::TAB << minAbsConRate
-            << token::TAB << sumSolverIter
-            << token::TAB << maxSolverIter
-            << token::TAB << pimple_iter;
-
-        const word resultName(fieldName);
-        setResult(resultName + "_initial", initialResidual);
-        setResult(resultName + "rate_avg", avgAbsConRate);
-        setResult(resultName + "rate_max", maxAbsConRate);
-        setResult(resultName + "rate_min", minAbsConRate);
-        setResult(resultName + "_sum_iters", sumSolverIter);
-        setResult(resultName + "_max_iters", maxSolverIter);
-        setResult(resultName + "_pimple_iters", pimple_iter);
-    }
-}
-
-
 void Foam::functionObjects::agentSolverSettings::writeFileHeader(Ostream& os, const word& fieldName)
 {
     if (!fieldSet_.updateSelection())
@@ -165,6 +82,98 @@ void Foam::functionObjects::agentSolverSettings::writeFileHeader(Ostream& os, co
     writtenHeader_ = true;
 }
 
+
+void Foam::functionObjects::agentSolverSettings::writeResidualsToFile(Ostream& os, const word& fieldName)
+{
+    file() << token::TAB << solverName_;
+
+    // for now only write these quantities to the file, later this will be additionally used as policy input
+    file()
+        << token::TAB << initialResidual_
+        << token::TAB << avgAbsConRate_
+        << token::TAB << maxAbsConRate_
+        << token::TAB << minAbsConRate_
+        << token::TAB << sumSolverIter_
+        << token::TAB << maxSolverIter_
+        << token::TAB << pimple_iter_;
+
+    const word resultName(fieldName);
+    setResult(resultName + "_initial", initialResidual_);
+    setResult(resultName + "rate_avg", avgAbsConRate_);
+    setResult(resultName + "rate_max", maxAbsConRate_);
+    setResult(resultName + "rate_min", minAbsConRate_);
+    setResult(resultName + "_sum_iters", sumSolverIter_);
+    setResult(resultName + "_max_iters", maxSolverIter_);
+    setResult(resultName + "_pimple_iters", pimple_iter_);
+
+    file() << endl;
+
+    // reset values after writing, otherwise there are summed up throughout all following time steps
+    sumSolverIter_ = 0;
+    maxSolverIter_ = 0;
+    maxAbsConRate_ = 0.0;
+    minAbsConRate_ = 42.0;
+}
+
+
+void Foam::functionObjects::agentSolverSettings::computeResidualProperties(const word& fieldName)
+{
+    const Foam::dictionary& solverDict = mesh_.solverPerformanceDict();
+
+    const List<SolverPerformance<scalar>> sp
+    (
+        solverDict.lookup(fieldName)
+    );
+
+    // same solver (GAMG) for all iterations within the time step, so just take the name of the first one
+    const word& solverName = sp[0].solverName();
+    const scalar& initialResidual = component(sp[0].initialResidual(), 0);
+
+    // don't know how to set the solverName and initialResidual directly, so for now assign the values to new variables
+    solverName_ = solverName;
+    initialResidual_ = initialResidual;
+
+    // we only have one GAMG call per PIMPLE iteration, otherwise computing the convergence rate would not work
+    pimple_iter_ = sp.size();
+
+    // convergence rate has one element less since we are computing the difference between two subsequent iterations
+    // TODO: check if we always have only one GAMG call per pimple iter, otherwise we can't compute the convergence rate correctly
+    convergenceRate_ = torch::zeros(pimple_iter_ - 1, torch::TensorOptions().dtype(torch::kFloat64));
+
+    // loop over the residuals of each GAMG call and compute the quantities of interest
+    for (label i = 0; i < pimple_iter_; i++){
+        // since we are only using p, we only have one component, which is the first one
+        const scalar nIterations = component(sp[i].nIterations(), 0);
+
+        // update the max. amount of iterations of GAMG per PIMPLE call
+        if (nIterations > maxSolverIter_){
+            maxSolverIter_ = nIterations;
+        }
+
+        // update sum of GAMG iterations
+        sumSolverIter_ += nIterations;
+
+        // since we are computing the difference between i and i+1 element, we have one idx less
+        if (i < pimple_iter_ - 1){
+            // compute the convergence rate
+            convergenceRate_[i] = component(sp[i].initialResidual(), 0) - component(sp[i+1].initialResidual(), 0);
+
+            // save the max. convergence rate, magnitude because convergence rate is negative
+            if (mag(convergenceRate_[i].item<double>()) > maxAbsConRate_){
+                maxAbsConRate_ = mag(convergenceRate_[i].item<double>());
+            }
+
+            // save the min. convergence rate, magnitude because convergence rate is negative
+            if (mag(convergenceRate_[i].item<double>()) < minAbsConRate_){
+                minAbsConRate_ = mag(convergenceRate_[i].item<double>());
+            }
+        }
+    }
+
+    // for simplicity, for now just compute the avg. convergence rate instead of median
+    avgAbsConRate_ = torch::mean(convergenceRate_).abs().item<double>();
+}
+
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
 Foam::functionObjects::agentSolverSettings::agentSolverSettings
@@ -178,7 +187,12 @@ Foam::functionObjects::agentSolverSettings::agentSolverSettings
     writeFile(obr_, name, typeName, dict),
     fieldSet_(mesh_),
     residualFieldNames_(),
-    initialised_(false)
+    initialised_(false),
+    train_(dict.get<bool>("train")),
+    policy_name_(dict.get<word>("policy")),
+    policy_(torch::jit::load(policy_name_)),
+    seed_(dict.get<int>("seed")),
+    gen_(seed_)
 {
     read(dict);
 }
@@ -206,7 +220,7 @@ bool Foam::functionObjects::agentSolverSettings::read(const dictionary& dict)
 bool Foam::functionObjects::agentSolverSettings::execute()
 {
     // we always have only the pressure field, which we want to use for our policy input, to first check if there is a
-    // field available and that exactly one field is specified. Otherwise, the agent wouldn't have a policy input crash
+    // field available and that exactly one field is specified. Otherwise, the agent wouldn't have a policy input and crash
     // TODO: maybe better to additionally exit here
     if (fieldSet_.size() < 1)
     {
@@ -242,12 +256,122 @@ bool Foam::functionObjects::agentSolverSettings::execute()
 
     writeCurrentTime(file());
 
-    // not compute the properties for the policy input of the residuals we are interested in
+    // now compute the properties for the policy input of the residuals we are interested in
     computeResidualProperties(fieldName);
 
-    file() << endl;
+    // predict solver settings for next time step, for now we will only consider 'interpolateCorrection'
+    predictSettings();
+
+    // modify the solver setting dict for the pressure using the predicted settings
+    modifySolverSettingsDict(fieldName);
+
+    // write the computed properties of the residuals to the dat-file in the 'postProcessing' directory
+    writeResidualsToFile(file(), fieldName);
 
     return true;
+}
+
+
+void Foam::functionObjects::agentSolverSettings::predictSettings()
+{
+    // this method is adopted from drlfoam, https://github.com/OFDataCommittee/drlfoam
+    if (Pstream::master()) // evaluate policy only on the master
+    {
+        // convert all ints to doubles since the feature vector will contain doubles
+        double pimple_iter = pimple_iter_;
+        double maxSolverIter = maxSolverIter_;
+        double sumSolverIter = sumSolverIter_;
+
+        // create feature vector, therefore we need to convert everything to double
+        std::vector<double> ft = {pimple_iter, maxSolverIter, sumSolverIter, avgAbsConRate_, initialResidual_,
+                                  maxAbsConRate_, minAbsConRate_};
+
+        // std::vector<double> ft = {pimple_iter, maxSolverIter, sumSolverIter};
+        const int nFeatures = ft.size();
+
+        torch::Tensor features = torch::from_blob(ft.data(), {1, nFeatures}).to(torch::kFloat64);
+
+        // make prediction
+        // TODO: policy input varies over ~6...8 orders of magnitude -> LayerNorm mitigates this issue -> see create_dummy_policy.py
+        std::vector<torch::jit::IValue> policyFeatures{features};
+        torch::Tensor policy_out = policy_.forward(policyFeatures).toTensor();
+
+        // convert prediction to scalar, at the moment the prediction contains only one value
+        scalar prob_out = policy_out[0][0].item<double>();
+
+        if (train_)
+        {
+            std::bernoulli_distribution distr(prob_out);
+            action_ = distr(gen_);
+        }
+
+        else
+        {
+            // convert the probability to a bool
+            if (prob_out <= 0.5)
+            {
+                action_ = 0;
+            }
+            else
+            {
+                action_ = 1;
+            }
+        }
+
+        // save the policy output, the execution time per time step is logged using the 'timeInfo' function object
+        saveTrajectory(prob_out);
+    }
+}
+
+
+void Foam::functionObjects::agentSolverSettings::modifySolverSettingsDict(const word& fieldName)
+{
+    // map the action to the settings, at the moment only for 'interpolateCorrection'
+    word interpolateCorrection;
+    if (action_)
+    {
+       interpolateCorrection = "yes";
+    }
+    else
+    {
+        interpolateCorrection = "no";
+    }
+
+    /* not working because apparently it is not possible to write the tokens of these dicts using the '<<' operator
+    // read the 'fvSolutions' file and find the dict corresponding to the target quantity, e.g. 'p' or 'p_rgh'
+    // we can't access the fvSolution directly, however, mesh_ is a 'volScalarField' which is derived from fvSolution
+    auto fvSolutionDict = mesh_.findObject<IOdictionary>("fvSolution");
+
+    // take the two dicts for solver settings and PIMPLE settings and save into new dict since the 'fvSolutionDict' is
+    // declared as const Foam::dictionary, we can't modify it directly, so we have to save everything and write it back
+    // to the file after modifications
+    dictionary solverDict = fvSolutionDict -> subDict("solvers");
+    dictionary pimpleDict = fvSolutionDict -> subDict("PIMPLE");
+
+    // update the dict, for now only with the 'interpolateCorrection' parameter (set is altering the settings internally)
+    // the values which are already present are overwritten by calling the set() method
+    solverDict.subDict(fieldName).set("interpolateCorrection", interpolateCorrection);
+
+    // merge for writing
+    dictionary fvSolutionDictMod;
+    fvSolutionDictMod.add("solvers", solverDict);
+    fvSolutionDictMod.add("PIMPLE", pimpleDict);
+
+    // update the fvSolution file
+    writeFvSolutionFile(fvSolutionDictMod);
+    */
+
+    // for now, we only modify 'interpolateCorrection', so just keep everything else const.
+    const word& solverSettings = "\t{\n"
+                                         "\t\tsolver \tGAMG;\n"
+                                         "\t\tsmoother \tDICGaussSeidel;\n"
+                                         "\t\ttolerance \t1e-06;\n"
+                                         "\t\trelTol \t0.01;\n"
+                                         "\t\tinterpolateCorrection \t" + interpolateCorrection + ";\n"
+                                 "\t}\n";
+
+    // update the fvSolution file
+    writeFvSolutionFile(solverSettings, fieldName, 5);
 }
 
 
@@ -286,5 +410,100 @@ bool Foam::functionObjects::agentSolverSettings::write()
     return true;
 }
 
+
+void Foam::functionObjects::agentSolverSettings::saveTrajectory(scalar prob_out) const
+{
+    // taken from drlfoam, https://github.com/OFDataCommittee/drlfoam
+    // for some reason values like 0.1, 0.2, ... are written out as 1, 2, ... if we write a csv file, but txt file works
+    std::ifstream file("trajectory.txt");
+    std::fstream trajectory("trajectory.txt", std::ios::app | std::ios::binary);
+    const word t = mesh_.time().timeName();
+
+    if(!file.good())
+    {
+        // write header
+        trajectory << "t, p, interpolateCorr";
+    }
+
+    trajectory << std::setprecision(15)
+               << "\n"
+               << t << ", "
+               << prob_out << ", "
+               << action_;
+}
+
+
+void Foam::functionObjects::agentSolverSettings::writeFvSolutionFile(const word& solverSettings,
+                                                                     const word& fieldName, const int nParameters) const
+{
+    // this function modifies the fvSolution dict, however this is very inefficient and just for testing purposes
+    // the frequent IO leads to crash in simulation because at some point the file is not written but OF already tries
+    // to read it
+    // TODO: use IO objects / dict to modify this file more efficiently and faster, further: file is read in 2 dt later... issue!
+    const fileName& systemDir = mesh_.time().system();
+
+    // for now just use backslash until i figured out how to assemble the path independently of os
+    std::ifstream file("./" + systemDir + "/" + "fvSolution");
+    std::fstream fvSolutionFile("./" + systemDir + "/" + "fvSolution");
+
+    // loop over the contents of the file, until we find our target field name
+    int counter = 0;
+    int start = 0;
+    word line;
+    word newFile;
+
+    while (std::getline(fvSolutionFile, line))
+    {
+        counter++;
+
+        // assuming the solvers dict always starts with the field of interest ,e.g. p or p_rgh
+        if (line.find("solvers") != std::string::npos)
+        {
+            newFile += line;
+            newFile += "\n";
+            newFile += "{\n\t";
+            newFile += fieldName;
+            newFile += "\n";
+            newFile += solverSettings;
+            newFile += "\n";                        // account for the additional entry
+            start = counter + nParameters + 4;      // len of dict + 2 lines for brackets 2 lines for line breaks
+            continue;
+        }
+
+        // omit the p-dictionary of the previous time step since it is already replaced
+        if (counter <= start && start > 0)
+        {
+            continue;
+        }
+
+        // once we omitted the original p-dict we can write the rest of the fvSolution
+        else if (counter > start && start > 0)
+        {
+            // we need to make sure there is always only 1 new line after the dict, otherwise we would add 1 new line
+            // each time step
+            if (counter == start+1 && line.empty())
+            {
+                continue;
+            }
+            // Write the line to the output file
+            newFile += line;
+            newFile += "\n";
+            continue;
+        }
+
+        // otherwise we haven't reached the solvers dict yet, so we just write the header etc. into the file
+        else
+        {
+            newFile += line;
+            newFile += "\n";
+        }
+    }
+    fvSolutionFile.close();
+
+    // now overwrite the fvSolution file with the new settings
+    std::ofstream fvSolutionFileOut("./" + systemDir + "/" + "fvSolution");
+    fvSolutionFileOut << newFile;
+    fvSolutionFileOut.close();
+}
 
 // ************************************************************************* //
